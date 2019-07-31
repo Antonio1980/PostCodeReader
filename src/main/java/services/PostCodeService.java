7c333c88086e23ed4413cc7d426eb03b88d8095f
antonio1980 <a220580>
2019-07-31 13:55:07 +0300

    fixed PostCodeService - properties moved to Service base

--- src/main/java/services/PostCodeService.java
+++ src/main/java/services/PostCodeService.java
@@ -4,7 +4,6 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.apache.commons.logging.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,10 +13,11 @@ import org.json.JSONObject;
  */
 public class PostCodeService extends ServiceBase{
 
-    String header_key = "Content-Type";
-    String header_value = "application/json";
-    private String baseUrl = "http://postcodes.io/postcodes/";
-    private Log log;
+    private String baseUrl;
+
+    public PostCodeService() {
+        this.baseUrl = super.baseUrl + "/postcodes/";
+    }
 
     /**
      *
@@ -29,7 +29,7 @@ public class PostCodeService extends ServiceBase{
         String uri = this.baseUrl.concat(postcode);
 
         System.out.println("API URL: " + uri);
-        return Unirest.get(uri).asJson().getBody().getObject();
+        return Unirest.get(uri).header(this.header_key, this.header_value).asJson().getBody().getObject();
     }
 
     /**
@@ -41,7 +41,7 @@ public class PostCodeService extends ServiceBase{
     public JSONObject getNearest(String postcode) throws UnirestException {
         String uri = this.baseUrl.concat(postcode).concat("/nearest");
         System.out.println(uri);
-        HttpResponse<JsonNode> jsonResponse = Unirest.get(uri).asJson();
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(uri).header(this.header_key, this.header_value).asJson();
         return jsonResponse.getBody().getObject();
     }
 
@@ -55,7 +55,7 @@ public class PostCodeService extends ServiceBase{
     public boolean isValid(String postcode) throws JSONException, UnirestException {
         String uri = this.baseUrl.concat(postcode).concat("/validate");
         System.out.println(uri);
-        return Unirest.get(uri).asJson()
+        return Unirest.get(uri).header(this.header_key, this.header_value).asJson()
                 .getBody().getObject().getBoolean("result");
     }
 
--- src/main/java/services/ServiceBase.java
+++ src/main/java/services/ServiceBase.java
@@ -5,9 +5,9 @@ package services;
  * @author Tanya Haus
  */
 class ServiceBase {
-
-    String baseUrl = "http://postcodes.io";
+    private String protocol = "http://";
     String header_key = "Content-Type";
     String header_value = "application/json";
+    String baseUrl = protocol + "postcodes.io";
 
 }//end of ServiceBase class
