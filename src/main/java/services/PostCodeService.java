47768e02ae34dc6d9c0f62534da817c9c084d92b
antonio1980 <a220580>
2019-07-31 14:12:58 +0300

    removed JsonNode redundant

--- src/main/java/services/PostCodeService.java
+++ src/main/java/services/PostCodeService.java
@@ -1,7 +1,5 @@
 package services;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
@@ -24,16 +22,16 @@ public class PostCodeService extends ServiceBase{
 
     public JSONObject getNearest(String postcode) throws UnirestException {
         String uri = this.baseUrl.concat(postcode).concat("/nearest");
+
         System.out.println(uri);
-        HttpResponse<JsonNode> jsonResponse = Unirest.get(uri).header(this.header_key, this.header_value).asJson();
-        return jsonResponse.getBody().getObject();
+        return Unirest.get(uri).header(this.header_key, this.header_value).asJson().getBody().getObject();
     }
 
     public boolean isValid(String postcode) throws UnirestException {
         String uri = this.baseUrl.concat(postcode).concat("/validate");
+
         System.out.println(uri);
-        return Unirest.get(uri).header(this.header_key, this.header_value).asJson()
-                .getBody().getObject().getBoolean("result");
+        return Unirest.get(uri).header(this.header_key, this.header_value).asJson().getBody().getObject().getBoolean("result");
     }
 
 }
